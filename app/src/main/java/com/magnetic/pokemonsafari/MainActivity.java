6dc2679e1d8dc6ca4b76941b9e9426dc1c03f79b
Joey Bickerstaff <joey.bickerstaff@magnetic.com>
2016-08-24 17:24:58 -0400

    stuff

--- app/src/main/java/com/magnetic/pokemonsafari/MainActivity.java
+++ app/src/main/java/com/magnetic/pokemonsafari/MainActivity.java
@@ -2,65 +2,166 @@ package com.magnetic.pokemonsafari;
 
 import android.app.Activity;
 import android.graphics.SurfaceTexture;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.TextureView.SurfaceTextureListener;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.widget.Toast;
 
-import dji.sdk.SDKManager.DJISDKManager;
+import dji.sdk.Camera.DJICamera;
+import dji.sdk.Camera.DJICamera.CameraReceivedVideoDataCallback;
+import dji.sdk.Codec.DJICodecManager;
+import dji.sdk.base.DJIBaseComponent.DJICompletionCallback;
+import dji.sdk.base.DJIBaseProduct;
+import dji.sdk.base.DJIBaseProduct.Model;
+import dji.sdk.base.DJIError;
+import dji.sdk.Camera.DJICameraSettingsDef.CameraMode;
+import dji.sdk.Camera.DJICameraSettingsDef.CameraShootPhotoMode;
 
-public class MainActivity extends Activity implements TextureView.SurfaceTextureListener, View.OnClickListener {
-    protected TextureView mVideoSurface = null;
-    private Button mCaptureBtn, mShootPhotoModeBtn, mRecordVideoModeBtn;
-    private ToggleButton mRecordBtn;
-    private TextView recordingTime;
+public class MainActivity extends Activity implements SurfaceTextureListener, OnClickListener {
+
+    private static final String TAG = MainActivity.class.getName();
+    protected DJICamera.CameraReceivedVideoDataCallback mReceivedVideoDataCallBack = null;
+
+    // Codec for video live view
+    protected DJICodecManager codecManager = null;
+
+    protected TextureView videoSurface = null;
+    private Button captureButton;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initUI();
+
+        // The callback for receiving the raw H264 video data for camera live view
+        mReceivedVideoDataCallBack = new CameraReceivedVideoDataCallback() {
+
+            @Override
+            public void onResult(byte[] videoBuffer, int size) {
+                if (codecManager != null) {
+                    // Send the raw H264 video data to codec manager for decoding
+                    codecManager.sendDataToDecoder(videoBuffer, size);
+                } else {
+                    Log.e(TAG, "codecManager is null");
+                }
+            }
+        };
+
+        DJICamera camera = PokemonSafariApplication.getCameraInstance();
+    }
+
+    protected void onProductChange() {
+        initPreviewer();
     }
 
     @Override
     public void onResume() {
+        Log.e(TAG, "onResume");
         super.onResume();
+        initPreviewer();
+        onProductChange();
+
+        if (videoSurface == null) {
+            Log.e(TAG, "videoSurface is null");
+        }
     }
 
     @Override
     public void onPause() {
+        Log.e(TAG, "onPause");
+        uninitPreviewer();
         super.onPause();
     }
 
     @Override
     public void onStop() {
+        Log.e(TAG, "onStop");
         super.onStop();
     }
 
     public void onReturn(View view) {
+        Log.e(TAG, "onReturn");
         this.finish();
     }
 
     @Override
     protected void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        uninitPreviewer();
         super.onDestroy();
+    }
+
+    private void initUI() {
+        // init videoSurface
+        videoSurface = (TextureView) findViewById(R.id.video_previewer_surface);
+        captureButton = (Button) findViewById(R.id.btn_capture);
+
+        if (null != videoSurface) {
+            videoSurface.setSurfaceTextureListener(this);
+        }
+
+        captureButton.setOnClickListener(this);
+    }
+
+    private void initPreviewer() {
+
+        DJIBaseProduct product = PokemonSafariApplication.getProductInstance();
+
+        if (product == null || !product.isConnected()) {
+            showToast(getString(R.string.disconnected));
+        } else {
+            if (null != videoSurface) {
+                videoSurface.setSurfaceTextureListener(this);
+            }
+            if (!product.getModel().equals(Model.UnknownAircraft)) {
+                DJICamera camera = product.getCamera();
+                if (camera != null) {
+                    // Set the callback
+                    camera.setDJICameraReceivedVideoDataCallback(mReceivedVideoDataCallBack);
+                }
+            }
+        }
+    }
+
+    private void uninitPreviewer() {
+        DJICamera camera = PokemonSafariApplication.getCameraInstance();
+        if (camera != null) {
+            // Reset the callback
+            PokemonSafariApplication.getCameraInstance().setDJICameraReceivedVideoDataCallback(null);
+        }
     }
 
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureAvailable");
+        if (codecManager == null) {
+            codecManager = new DJICodecManager(this, surface, width, height);
+        }
     }
 
     @Override
     public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureSizeChanged");
     }
 
     @Override
     public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        Log.e(TAG, "onSurfaceTextureDestroyed");
+        if (codecManager != null) {
+            codecManager.cleanSurface();
+            codecManager = null;
+        }
+
         return false;
     }
 
@@ -68,47 +169,49 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
     }
 
-    private void initUI() {
-        // init mVideoSurface
-        mVideoSurface = (TextureView) findViewById(R.id.video_previewer_surface);
-        recordingTime = (TextView) findViewById(R.id.timer);
-        mCaptureBtn = (Button) findViewById(R.id.btn_capture);
-        mRecordBtn = (ToggleButton) findViewById(R.id.btn_record);
-        mShootPhotoModeBtn = (Button) findViewById(R.id.btn_shoot_photo_mode);
-        mRecordVideoModeBtn = (Button) findViewById(R.id.btn_record_video_mode);
-
-        if (null != mVideoSurface) {
-            mVideoSurface.setSurfaceTextureListener(this);
-        }
-
-        mCaptureBtn.setOnClickListener(this);
-        mRecordBtn.setOnClickListener(this);
-        mShootPhotoModeBtn.setOnClickListener(this);
-        mRecordVideoModeBtn.setOnClickListener(this);
-        recordingTime.setVisibility(View.INVISIBLE);
-        mRecordBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
     public void onClick(View v) {
+
         switch (v.getId()) {
             case R.id.btn_capture: {
+                captureAction();
                 break;
             }
-            case R.id.btn_shoot_photo_mode: {
-                break;
-            }
-            case R.id.btn_record_video_mode: {
-                break;
-            }
+
             default:
                 break;
         }
     }
 
+    // Method for taking photo
+    private void captureAction() {
+
+        CameraMode cameraMode = CameraMode.ShootPhoto;
+
+        final DJICamera camera = PokemonSafariApplication.getCameraInstance();
+        if (camera != null) {
+
+            CameraShootPhotoMode photoMode = CameraShootPhotoMode.Single; // Set the camera capture mode as Single mode
+            camera.startShootPhoto(photoMode, new DJICompletionCallback() {
+
+                @Override
+                public void onResult(DJIError error) {
+                    if (error == null) {
+                        showToast("take photo: success");
+                    } else {
+                        showToast(error.getDescription());
+                    }
+                }
+
+            }); // Execute the startShootPhoto API
+        }
+    }
 }
--- app/src/main/java/com/magnetic/pokemonsafari/PokemonSafariApplication.java
+++ app/src/main/java/com/magnetic/pokemonsafari/PokemonSafariApplication.java
@@ -1,10 +1,156 @@
 package com.magnetic.pokemonsafari;
 
 import android.app.Application;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
 
-public class PokemonSafariApplication extends Application{
+import dji.sdk.Camera.DJICamera;
+import dji.sdk.Products.DJIAircraft;
+import dji.sdk.Products.DJIHandHeld;
+import dji.sdk.SDKManager.DJISDKManager;
+import dji.sdk.base.DJIBaseComponent;
+import dji.sdk.base.DJIBaseComponent.DJIComponentListener;
+import dji.sdk.base.DJIBaseProduct;
+import dji.sdk.base.DJIBaseProduct.DJIBaseProductListener;
+import dji.sdk.base.DJIBaseProduct.DJIComponentKey;
+import dji.sdk.base.DJIError;
+import dji.sdk.base.DJISDKError;
+
+public class PokemonSafariApplication extends Application {
+
+    public static final String FLAG_CONNECTION_CHANGE = "fpv_tutorial_connection_change";
+
+    private static DJIBaseProduct djiBaseProduct;
+
+    private Handler mainLooper;
+
+    /**
+     * This function is used to get the instance of DJIBaseProduct.
+     * If no product is connected, it returns null.
+     */
+    public static synchronized DJIBaseProduct getProductInstance() {
+        if (null == djiBaseProduct) {
+            djiBaseProduct = DJISDKManager.getInstance().getDJIProduct();
+        }
+        return djiBaseProduct;
+    }
+
+    public static boolean isAircraftConnected() {
+        return getProductInstance() != null && getProductInstance() instanceof DJIAircraft;
+    }
+
+    public static boolean isHandHeldConnected() {
+        return getProductInstance() != null && getProductInstance() instanceof DJIHandHeld;
+    }
+
+    public static synchronized DJICamera getCameraInstance() {
+
+        if (getProductInstance() == null) return null;
+        return getProductInstance().getCamera();
+
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mainLooper = new Handler(Looper.getMainLooper());
+        //This is used to start SDK services and initiate SDK.
+        DJISDKManager.getInstance().initSDKManager(this, mDJISDKManagerCallback);
     }
+
+    /**
+     * When starting SDK services, an instance of interface DJISDKManager.DJISDKManagerCallback will be used to listen to
+     * the SDK Registration result and the product changing.
+     */
+    private DJISDKManager.DJISDKManagerCallback mDJISDKManagerCallback = new DJISDKManager.DJISDKManagerCallback() {
+
+        //Listens to the SDK registration result
+        @Override
+        public void onGetRegisteredResult(DJIError error) {
+
+            if (error == DJISDKError.REGISTRATION_SUCCESS) {
+
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Register Success", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                DJISDKManager.getInstance().startConnectionToProduct();
+
+            } else {
+
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Register sdk fails, check network is available", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+            Log.e("TAG", error.toString());
+        }
+
+        //Listens to the connected product changing, including two parts, component changing or product connection changing.
+        @Override
+        public void onProductChanged(DJIBaseProduct oldProduct, DJIBaseProduct newProduct) {
+
+            djiBaseProduct = newProduct;
+            if (djiBaseProduct != null) {
+                djiBaseProduct.setDJIBaseProductListener(mDJIBaseProductListener);
+            }
+
+            notifyStatusChange();
+        }
+    };
+
+    private DJIBaseProductListener mDJIBaseProductListener = new DJIBaseProductListener() {
+
+        @Override
+        public void onComponentChange(DJIComponentKey key, DJIBaseComponent oldComponent, DJIBaseComponent newComponent) {
+
+            if (newComponent != null) {
+                newComponent.setDJIComponentListener(mDJIComponentListener);
+            }
+            notifyStatusChange();
+        }
+
+        @Override
+        public void onProductConnectivityChanged(boolean isConnected) {
+
+            notifyStatusChange();
+        }
+
+    };
+
+    private DJIComponentListener mDJIComponentListener = new DJIComponentListener() {
+
+        @Override
+        public void onComponentConnectivityChanged(boolean isConnected) {
+            notifyStatusChange();
+        }
+
+    };
+
+    private void notifyStatusChange() {
+        mainLooper.removeCallbacks(updateRunnable);
+        mainLooper.postDelayed(updateRunnable, 500);
+    }
+
+    private Runnable updateRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
+            sendBroadcast(intent);
+        }
+    };
+
 }
